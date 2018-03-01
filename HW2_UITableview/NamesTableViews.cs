bc33e3e418d4c8be33c08ef03be55e9b1145f256
james_mbp <jameskrauser@gmail.com>
2018-03-01 11:31:05 +0800

    add

---
+++ HW2_UITableview/NamesTableViews.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using UIKit;
+
+namespace HW2_UITableview
+{
+    public class NamesTableViews: UITableViewSource
+    {
+        private List<string> names;
+        /*
+        public NamesTableViews()
+        {
+        }
+        */
+        public NamesTableViews(List<string> names)
+        {
+            this.names = names;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            // throw new NotImplementedException(); 
+            var cell = new UITableViewCell(UITableViewCellStyle.Default, "");
+            cell.TextLabel.Text = names[indexPath.Row];
+            return cell;
+
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            //throw new NotImplementedException();
+            return names.Count;
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            //base.RowSelected(tableView, indexPath);
+            var selectedName = names[indexPath.Row];
+
+            Console.WriteLine("selectedName= {0} ", selectedName); 
+
+        }
+    }
+}
---
+++ HW2_UITableview/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace HW2_UITableview
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            List<string> names = new List<string>
+            {  "1","2","3","4","5",
+                "6","7","8","9","10",
+                "10","11","12","13","14"
+            };
+            NamesTableView.Source =
+                              new NamesTableViews(names);
+
+
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
